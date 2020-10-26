4d9b202a4c1c7982c28bde90f515d6e90674d1f5
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2020-10-26 15:43:14 +0800

    建立主樣板：新增layouts資料夾&新增、編輯 master.blade.php內容

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,13 @@
+
+<html>
+    <head>
+        <title>@yield('title')|MyBlog</title>
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
+
+

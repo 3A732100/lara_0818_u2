d9a8ff9be1a5189bf4de0b5bda3fb4651a06d66c
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2020-11-28 13:26:01 +0800

    更正 練習2-2

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-    <h1>Home</h1>>
-    <p>This is my body content.</p>>
-</head>
-<body>
-
-</body>
-</html>
-
-
 @extends('layouts.master')
 @section('title','Home')
 @section('content')

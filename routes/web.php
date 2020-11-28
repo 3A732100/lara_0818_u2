b736e40977fdd9c709fa0937927280d13d0dfc37
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2020-11-28 13:16:09 +0800

    更正 練習1-8

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,14 @@ use App\Http\Controllers\HelloController;
 */
 
 //原根目錄
-//Route::get('/', function () {return view('welcome');});
+Route::get('/', function () {return view('welcome');});
 
 //Route::get('home',function(){return view('home');});
 
-Route::get('home',function(){
-    return view('home.index');
-});
+//Route::get('home',function(){return view('home.index');});
 
-Route::get('/',[HomeController::class,'index']);
 
+Route::get('home',[HelloController::class,'index'])->name('home.index');
 
 
 Route::get('hello/{name?}',[HelloController::class,'index'])->name('hello.index');
